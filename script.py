6b8e4b4dd5eefbd21898fae490ed187f475cf2b2
Stefania Ke <stefania_ke@hotmail.com>
2017-11-09 14:00:00 +0100

    I imported test into script

--- script.py
+++ script.py
@@ -1,1 +1,11 @@
+
 print('mijn naam is stefania')
+
+
+import test
+
+test.circumf_circle(2)
+test.surf_area_circle(2)
+
+print('The circumference of the circle is', test.circumf_circle(2))
+print('The surface area of the circle is', test.surf_area_circle(2))
